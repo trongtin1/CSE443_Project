946cec9e45021c5e92c93b931bb85c475fec7814
yanyanxd <hoang.luong.cit20@eiu.edu.vn>
2025-06-07 23:54:40 +0700

    revert dashboardviewmodel

---
+++ Admin/Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CSE443_Project.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalJobs { get; set; }
+    }
+}
